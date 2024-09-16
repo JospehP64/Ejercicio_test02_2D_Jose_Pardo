197325532d2c60a67e0a6f57f036e0d827e6e4a8
JospehP64 <jmjakepardo@gmail.com>
2024-09-16 18:22:32 +0200

    16-09-2024
    -New script changes ( until the exercise 2.8)

--- Assets/scripts/Ejercicio_2_1.cs
+++ Assets/scripts/Ejercicio_2_1.cs
@@ -7,9 +7,8 @@ public class Ejercicio_2_1 : MonoBehaviour
 {
     //RECUERDA: usa siempre [SerializeField] en vez de public en una variable o método que quieras usar (salvo por casos especiales)
 
-    int puntosDeValentia = 12;
-    int puntosDeIntuicion = 14;
-    int puntosDeInteligencia = 15;
+    int Monedas;
+    int horas = 0, minutos = 0, segundos = 0, milisegundos = 0;
 
     int vidasPlayer1 = 1;
     int vidasPlayer2 = 2;
@@ -30,7 +29,7 @@ public class Ejercicio_2_1 : MonoBehaviour
     float peso = 5.75f;
     bool AlumnoRepetidor = false;
     char letra = 'A';
-    int minutos = 5;//sin los segundos
+    
     bool MatriculaCoche = false;
     bool MayorDeEdad = false;
     int CodigoPostal = 28999;
@@ -97,10 +96,96 @@ public class Ejercicio_2_1 : MonoBehaviour
 
         Debug.Log("-----------------------------");
         Debug.Log("vidas de player4= " + vidasPlayer4);
-        //IMPORTANTE (EJERCICIO 2.6): PARA CALCULAR LA MEDIA, DEBES SUMAR TODOS LOS VALORES Y DIVIDIR EL RESULTADO POR EL NUMERO DE VALORES
+        //TERMINAR_IMPORTANTE (EJERCICIO 2.6): PARA CALCULAR LA MEDIA, DEBES SUMAR TODOS LOS VALORES Y DIVIDIR EL RESULTADO POR EL NUMERO DE VALORES
+        int puntosDeValentia = 12;
+        int puntosDeIntuicion = 14;
+        int puntosDeInteligencia = 15;
+        int sumaPuntos;
+        int puntuacionTotal;
+
+        puntuacionTotal = (puntosDeValentia + puntosDeIntuicion + puntosDeInteligencia);
+
+        Debug.Log("");
 
 
 
+
+        //EJERCICIO 6.1, OPERADORES LÓGICOS 
+        //a)
+        //Se cumple solo si X es verdadero e Y es falso o si X es falso y Z es verdadero
+
+        //b) 
+        //solo se cumple si X es verdadero, Y es falso o  X es verdadero y Z es falso y Y es falso
+
+        //c)
+        //Solo se cumple si X es verdadero o si Y es falso y Z es verdadero
+
+        //d)primero se comrobaría si X es verdadero o Y es falso; después, si X es falso o si Z es verdadero
+        //y, por último, se comprobaría si las dos condiciones anteriores, se cumplen.
+
+        //Si en la ultima condición una de ellas no se cumple, no se puede ejecutar el contenido.
+
+        //e)Solo se cumle si X es verdadero, Y es falso o si X es verdadero y Z es falso y Y verdadero
+
+        //f) Solo se cumple si X es falso, Y es verdadero o Z es verdaderoy X es verdadero y Y es verdadero
+
+        //EJERCICIO 2.7: Crear un programa que te diga el número de monedas obtenido en 4 horas (usar variables para moneda y para el tiempo medido)
+
+        if (horas == 4)
+        {
+            Debug.Log("has conseguido " + Monedas + " monedas durante " + horas + " horas.");
+
+        }
+        else
+        {
+            //Debug.Log("horas: " + horas + ", minutos: " + minutos + ", segundos: " + segundos + " milisegundos: " + milisegundos);
+            Debug.Log("Monedas obtenidas: " + Monedas);
+            if (milisegundos >= 60)
+            {
+                segundos += 1;
+                Monedas += 1;
+                milisegundos = 0;
+                if (segundos >= 60)
+                {
+                    minutos += 1;
+                    segundos = 0;
+                }
+                if (minutos >= 60)
+                {
+                    horas += 1;
+                    minutos = 0;
+                }
+            }
+            else
+            {
+                milisegundos += 1;
+
+            }
+        }
+
+        //EJERCICIO 2.8:
+        int vida_rpg = 120;
+        int veneno_en_porcentaje = 3;
+        int venenoAtaque;
+        int numeroDeTurno = 0;
+
+        while (vida_rpg > 0)
+        {
+            Debug.Log("¡Has sido envenenado!");
+            venenoAtaque = (vida_rpg * veneno_en_porcentaje) / 100;
+            vida_rpg = vida_rpg - venenoAtaque;
+            Debug.Log("¡Has recibido daño! Te quedan " + vida_rpg + " puntos de vida");
+            numeroDeTurno++;
+            veneno_en_porcentaje *= 2;
+        }
+        if (vida_rpg <= 0)
+        {
+            Debug.Log("has muerto...");
+        }
+
+        //RECUERDA: para poder calcular un numero par o impar, tienes que usar en if el  cociente (%) (el resto sería /) de esta forma if (variable % dato == dato)
+        //por ejemplo: cuando usas "if (dias % 7 == 1)", estás comprobando el número impar de la semana (es decir, el martes, lunes); pero, cuando usas "if (días % 7 == 0), estás comprobando el número par de la semana (domingo)
+        
 
 
     }
@@ -109,8 +194,18 @@ public class Ejercicio_2_1 : MonoBehaviour
     void Update()//update y start se ejecutan automáticamente; el resto de métodos no lo hacen
     {
         //Debug.Log("Ha pasado un frame");//Este mensaje aparece porque, constantemente, esta función se actualiza
-        revive(3, "veneno");//este "metodo" se puede poner en el update, pero no es necesario.
-        //
+        //revive(3, "veneno");//este "metodo" se puede poner en el update, pero no es necesario.
+
+        
+        
+
+
+
+
+
+
+
+
 
     }
     int revive(int amenaza = 1, string estado = "nulo", int rondas = 0)//se ejecutará cuando lo llammes. Puedes declarar una variable local, la cual solo se usará en este método.

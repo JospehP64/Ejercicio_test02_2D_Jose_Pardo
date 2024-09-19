42e26a2df92588e6439e973e23bb48d8c654cb26
JospehP64 <jmjakepardo@gmail.com>
2024-09-19 21:24:09 +0200

    19-09-20204.7
    -Creado scripts para el ejercicio 8
    -TERMINAR TODOS LOS EJERCICIOS HASTA EL 8 EJERCICIO
    
    -REPASAR CONCEPTOS APRENDIDOS EN CLASE

---
+++ Assets/scripts/CreadorDePersonajes.cs
@@ -0,0 +1,23 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CreadorDePersonajes : MonoBehaviour
+{
+    
+    [SerializeField] Personaje finn;//he creado, de la clase (o script) un componente
+
+    void prepararPersonajes()
+    {
+
+    }
+
+    //#region sirve para establecer una region o una "carpeta" que contenga codigo del script
+    #region
+    //
+    ///
+    ///
+    ///
+    #endregion
+}
---
+++ Assets/scripts/Personaje.cs
@@ -0,0 +1,40 @@
+
+using JetBrains.Annotations;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Personaje : MonoBehaviour
+{
+    
+    private string name;
+    private float life;
+    private float speed;
+    private int exp;
+
+    
+
+
+    void lifes()
+    {
+
+    }
+    void speed_up()
+    {
+
+    }
+    void move()
+    {
+
+    }
+    void attack()
+    {
+
+    }
+    int calcularNivel()
+    {
+        int lvl = exp / 1000;
+        return lvl;
+    }
+    
+}
